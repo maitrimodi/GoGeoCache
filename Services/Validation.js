7e1e912b9d2b50a68994bbebd8ac1d294f3989f6
himaniJsavani <81823283+himaniJsavani@users.noreply.github.com>
2021-08-02 13:43:45 -0400

    add firebase code added

--- Services/Validation.js
+++ Services/Validation.js
@@ -1,6 +1,6 @@
 const regexExp = {
     latLng: /^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$/,
-    cashDetails: /^.{10,}$/
+    cacheDetails: /^.{10,}$/
 }
 
 export const validate = (text, regex) => {
